c17957570d110420aee27bc548b0a61336600327
fbyn4 <faiqalkautsarfabyan@gmail.com>
2026-07-22 21:33:20 +0700

    test git bian

---
+++ main.py
@@ -0,0 +1,69 @@
+a = 10
+
+print ("nilai a adalah", a)
+print ("nilai a adalah", a)
+
+data_siswa = ["andi", "budi", "caca"]
+print ("data", data_siswa)
+print ("bertipe", type(data_siswa))
+
+data_float = 1.5
+print ("data", data_float)
+print ("bertipe", type(data_float))
+
+data_string = "hello world"
+print ("data", data_string)
+print ("bertipe", type(data_string))
+
+data_string = "fabyan"
+print ("data", data_string)
+print ("bertipe", type(data_string))
+
+data_bool = True
+print ("data", data_bool)
+print ("bertipe", type(data_bool))
+
+data_compleks = complex(5,6)
+print ("data", data_compleks)
+print ("bertipe", type(data_compleks))
+
+
+x = str(4)
+y = int(4)
+z = float(4)
+
+print (x)
+print (y)
+print (z)
+
+
+from ctypes import c_double
+
+data_c_double = c_double(1.5)
+print ("data", data_c_double)
+print ("bertipe", type(data_c_double))
+
+data_float = float(0)
+print ("data", data_float, "bertipe", type(data_float))
+
+data_int = int(data_float)
+print ("data", data_int, "bertipe", type(data_int))
+data_bool = bool(data_float)
+print ("data", data_bool, "bertipe", type(data_bool))
+data_str = str(data_float)
+print ("data", data_str, "bertipe", type(data_str))
+
+data_bool = False
+print ("data", data_bool, "bertipe", type(data_bool))
+
+data_int = int(data_bool)
+data_float = float (data_bool)
+data_str = str(data_bool)
+print ("data", data_int, "bertipe", type(data_bool))
+print ("data", data_float, "bertipe", type(data_bool))
+print ("data", data_str, "bertipe", type(data_bool))
+
+
+
+       
+    
---
+++ your.py
@@ -0,0 +1,139 @@
+x = 8 
+y = 9.3
+z = 5j
+
+
+a = float(x)
+
+b = int(y)
+
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))      
+print(type(c))
+
+import random
+print(random.randrange(1, 10))
+
+
+x = 3
+y = 4.2
+z = 2 + 4j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+X = int(7)
+y = int(3.5)
+z = int(2)
+w = int("23")
+
+print(X)
+print(y)                
+print(z)
+print(w)
+
+
+# LATIHAN 
+
+x = 2
+
+a = float(x)
+b = str(x)
+
+print(a)
+print(b)
+
+import random
+print(random.randrange(1, 1000))
+
+
+for x in "Fabyan Faiq Alkautsar":
+    print(x)
+
+b = "Sypadia Ramadhani"
+print(b[0:16])
+
+x = "elysia"
+print(x[-3:-1])
+
+a = "fabyan faiq alkautsar" 
+print(a.upper())
+
+b = "Sypadia Ramadhani"
+print(b.lower())
+
+x = "fabyan faiq "
+print(x.replace("f", "p"))
+
+x = "aku adalah fabyan"
+print(x.split(" "))
+
+a = "hallo"
+b = "world"
+c = a + " " + b
+print(c)
+
+age = 80*12
+txt = f"my name is fabyan and i am {age} years old"
+print(txt)
+
+a = 45
+txt = f"the price is {a :.2f} dollars"
+print(txt)
+
+txt = f"the price is {45*80} dollars"
+print(txt)
+
+a = 23*80  
+txt = f"jadi harga barangnya adalah {a} rupiah"
+print(txt)
+
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt) 
+
+x = "FABYAN FAIQ ALKAUSAR" 
+print(x.casefold())
+
+X = "fabyan faiq alkautsar"
+print(X.capitalize())
+
+X = "Fabyan"
+print(X.center(70))
+
+x = "fabyan "
+print(x.count("y"))
+
+x = "fabyan"
+print(x.encode())
+
+x = "qiana"
+print(x.endswith("a"))
+
+x = "Nama:\tfabyan\tfaiq\talkautsar"
+print(x.expandtabs(2))
+
+x = "Fabyan"
+print(x.find("a"))
+
+nama = "fabyan"
+age = 18
+hehe = "haloo nama saya {} dan aku berumur {} tahun"
+print(hehe.format(nama,age))
+
+
+saudara2 = "saya adalah sudara {NAME1}, {UMUR1}, {Hobi}".format(NAME1="fabian", UMUR1="18", Hobi="Main hsr") 
+print(saudara2)
+
+
+
+
+
+
